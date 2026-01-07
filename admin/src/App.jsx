f78e8b3abd64c8268f0e8d9ce47198ff8d149c4e
harsh <harshad107@gmail.com>
2026-01-07 23:28:36 +0530

    fix vite base path for render

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -59,7 +59,7 @@ const App = () => {
   }, [token]);
 
   return (
-    <div className="min-h-screen bg-gray-100">
+    <div className="min-h-screen bg-gray-600">
       <ToastContainer
         position="top-right"
         autoClose={5000}
