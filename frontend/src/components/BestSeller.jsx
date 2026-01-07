93c221800b87b2728cc8f47c19815567d18fa5e6
harsh <harshad107@gmail.com>
2026-01-07 22:18:34 +0530

    fix vite base path for render

--- frontend/src/components/BestSeller.jsx
+++ frontend/src/components/BestSeller.jsx
@@ -18,10 +18,10 @@ const BestSeller = () => {
     <div className="my-10">
       <div className="py-8 text-3xl text-center">
         <Title text1={"BEST"} text2={"SELLERS"} />
-        {/* <p className="w-3/4 m-auto text-xs text-gray-600 sm:text-sm md:text-base">
+        <p className="w-3/4 m-auto text-xs text-gray-600 sm:text-sm md:text-base">
           Our best sellers are a curated selection of top-rated items that have
           won over shoppers with their quality, style, and value.
-        </p> */}
+        </p>
       </div>
       <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-y-6">
         {bestSeller.map((item, index) => (
